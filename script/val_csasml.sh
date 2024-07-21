1fc2392aa4afbab1f41ee90230a526cf04da2258
azaz0846 <58462641+azaz0846@users.noreply.github.com>
2024-07-21 15:57:57 +0800

    Delete script/val_csasml.sh

--- script/val_csasml.sh
+++
@@ -1,6 +0,0 @@
-DATA_PATH=/home/workspace/CSASML/cifar100/
-CODE_PATH=/home/workspace/CSASML/ # modify code path here
-
-
-cd $CODE_PATH && python validate.py $DATA_PATH --model csasml_s18  --checkpoint '/home/data/pyliao/CSASML/output/20240618-093545-pyformer_s18-224/model_best.pth.tar' --dataset 'torch/cifar100' --batch-size 128 --results-file val_result.csv --throughput --latency --output /home/data/pyliao/CSASML/output/20240618-093545-pyformer_s18-224/
-
